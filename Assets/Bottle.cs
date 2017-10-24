34465fb8559b2dee399f689ff8ffb84db1ff3021
SAE\bjohnston3 <bradycjohnston@hotmail.com>
2017-10-24 11:28:14 +1000

    Shaker is Shaking

---
+++ Assets/Bottle.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bottle : MonoBehaviour {
+
+    public bool pouring;
+    public bool pickedUp;
+
+    public Quaternion pourAngle;
+    public Quaternion currentAngle;
+
+    public Contents currentContent;
+
+    public enum Contents
+    {
+        //insert drink types
+    };
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Tilt()
+    {
+        //changes the angle of the bottle based on the input
+    }
+
+    public void Pour()
+    {
+        //tests if the current angle is greater that the pour angle
+    }
+
+    public bool IsPickedUp()
+    {
+        //tests if the bottle is picked up based on input
+        return false;
+    }
+
+    public void SetContent()
+    {
+        //idk
+    }
+}
---
+++ Assets/Shaking.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaking : MonoBehaviour
+{
+
+    public float baseRotateAngle = 5.0f;
+
+    private float timerInitial = 10.0f;
+    private float timeSinceLastPress = 0.0f;
+
+    public bool isPickedUp = false;
+    public bool isPouring = false;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        timeSinceLastPress = timerInitial;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        timeSinceLastPress += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Shake();
+        }
+	}
+
+    void Shake()
+    {
+        //get the opposite base angle
+        baseRotateAngle *= -1;
+        //set the angle
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f,0.0f,baseRotateAngle));
+    }
+}
