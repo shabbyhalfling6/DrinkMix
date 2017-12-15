ac675af732286df70bc71fcf0ceb6cb51089c15c
Brady Johnston <bradyjohnston22@gmail.com>
2017-12-15 12:57:19 +1000

    More Fixes and Itch.io Builds

--- Assets/GameFolder/Scripts/SerialHolder.cs
+++ Assets/GameFolder/Scripts/SerialHolder.cs
@@ -10,17 +10,34 @@ public class SerialHolder : MonoBehaviour {
 	public static float[] angle=new float[4];
 
 	public string ComPort = "COM6";
-	// Use this for initialization
-	void Start () {
-        System.IO.StreamReader sr = new System.IO.StreamReader(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\serial.txt");
-        int port = 6;
+    public int port = 22;
+    // Use this for initialization
+    void Start () {
+        System.IO.StreamReader sr;
+
+        try
+        {
+            sr = new System.IO.StreamReader(System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\serial.txt");
+        }
+        catch
+        {
+            serPortOpen = false;
+            return;
+        }
+
+        
         int.TryParse(sr.ReadLine(), out port);
         ComPort = "COM" + port;
         Debug.Log(ComPort);
 		serPort = new SerialPort(ComPort, 9600);
 
-		//tests if the serial port can be connected to
-		try
+        if (port == 22)
+        {
+
+        }
+
+        //tests if the serial port can be connected to
+        try
 		{
 			serPort.Open();
 		}
